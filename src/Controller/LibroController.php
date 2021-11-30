43d3321d912a657897e059ce5b56cb8269a2fe1a
Javier Gil <franciscojavier.gil@upm.es>
2021-11-30 10:43:32 +0100

    symfony console make:crud Libro

---
+++ src/Controller/LibroController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Libro;
+use App\Form\LibroType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/libro')]
+class LibroController extends AbstractController
+{
+    #[Route('/', name: 'libro_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $libros = $entityManager
+            ->getRepository(Libro::class)
+            ->findAll();
+
+        return $this->render('libro/index.html.twig', [
+            'libros' => $libros,
+        ]);
+    }
+
+    #[Route('/new', name: 'libro_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $libro = new Libro();
+        $form = $this->createForm(LibroType::class, $libro);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($libro);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('libro_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('libro/new.html.twig', [
+            'libro' => $libro,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'libro_show', methods: ['GET'])]
+    public function show(Libro $libro): Response
+    {
+        return $this->render('libro/show.html.twig', [
+            'libro' => $libro,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'libro_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Libro $libro, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(LibroType::class, $libro);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('libro_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('libro/edit.html.twig', [
+            'libro' => $libro,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'libro_delete', methods: ['POST'])]
+    public function delete(Request $request, Libro $libro, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$libro->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($libro);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('libro_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/LibroType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Libro;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class LibroType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titulo')
+            ->add('autor')
+            ->add('numPaginas')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Libro::class,
+        ]);
+    }
+}
