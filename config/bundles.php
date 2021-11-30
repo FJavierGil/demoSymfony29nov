e136bd06432b614da4f921dd0df8083ec14e7622
Javier Gil <franciscojavier.gil@upm.es>
2021-11-30 20:20:59 +0100

    composer require --dev doctrine/doctrine-fixtures-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
